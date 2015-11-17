b3eeb9695fffdfc8d08cf89883cd7f4777c70570
Daniel <daniel.kwak.dk@gmail.com>
2015-11-17 15:22:15 -0500

    Login page fix working on server now

--- authorize.php
+++ authorize.php
@@ -5,7 +5,18 @@ $user = new Memcached();
 if (null !=$user->get('id')) {
 echo "test";
 }else{
-    header("Location: index.html");
+    header("Location: Login.php");
 }
+
+if ("student"==$user->get('type')) {
+	# code...
+}elseif ("faculty"==$user->get('type')) {
+	# code...
+}elseif ("admin"==$user->get('type')) {
+	# code...
+}elseif ("researcher"==$user->get('type')) {
+	# code...
+}
+
 
 ?>
