4d8eb312e63f7dfb70e2ff25f10db17a417bd207
zachary chennis <zchennis32@gmail.com>
2015-11-19 13:36:57 -0500

    added faculty html/php

---
+++ faculty.php
@@ -0,0 +1,14 @@
+<?php
+require 'vendor/autoload.php';
+$loader = new Twig_Loader_Filesystem("views");
+$twig = new Twig_Environment($loader);
+$user = new Memcached();
+$id=$user->get('id');
+
+
+echo $twig->render('faculty.html');
+
+
+
+
+?>
