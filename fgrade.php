cff6f62556e03e2043c6bf27bcd1837b63195150
zachary chennis <zchennis32@gmail.com>
2015-11-24 14:33:37 -0500

    added course catalog, doesn't work

--- fgrade.php
+++ fgrade.php
@@ -3,11 +3,20 @@
 require 'vendor/autoload.php';
 $loader = new Twig_Loader_Filesystem("views");
 $twig = new Twig_Environment($loader);
+if(isset($_POST['btn-login'])){}
 $user = new Memcached();
 $id=$user->get('id');
+$type=$user->get('type');
+
+//$sql=$db->prepare("SELECT * FROM Registration.test;");
+$sql->execute();
+
+$result=$sql->fetchAll(PDO::FETCH_ASSOC);
 
 
-echo $twig->render('fgrade.html');
+echo $twig->render('fgrade.html', array('id'=>$id, 'type' => $type, 'data'=> $result ));
+
+
 
 
 
--- finfo.php
+++ finfo.php
@@ -9,17 +9,18 @@ $type=$user->get("type");
 
 
 echo $twig->render('finfo.html', array("id" =>$id, "type" => $type));
-);
+
 
 
 $id= $user->get("id");
 $db = "SELECT * FROM Registration.User WHERE" +$id +"" ;
 if($result->num_rows > 0){
     while($row = $result->fetch_assoc()){
-        echo "<br> First name: " $row["fname"] ".""<br>"; 
+        echo "<br> First name: " +$row["fname"]+ ".<br>"; 
     }
+   }
     else {
         echo "0 results";
     }
-}
+
 ?>
