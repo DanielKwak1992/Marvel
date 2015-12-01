8c1d76e84a7a3b21750caa800c7e8978fc4bc498
Daniel <daniel.kwak.dk@gmail.com>
2015-12-01 16:31:09 -0500

    Faculty changes

--- fgrade.php
+++ fgrade.php
@@ -1,35 +1,43 @@
 <?php
-//include_once "connection.php";
+include_once "connection.php";
 require 'vendor/autoload.php';
 $loader = new Twig_Loader_Filesystem("views");
 $twig = new Twig_Environment($loader);
 $user = new Memcached();
+//memcache only stores this data any other data you need will need to be queried
+//previously you had other variables that wherent in memcache
 $id=$user->get('id');
+$email=$user->get('email');
+$fname=$user->get('fname');
+$lname=$user->get('lname');
 $type=$user->get('type');
-$result=$user->get('result');
-$course=$user->('course');
-$CourseID=$user->get('CourseID');
-$Grade=$user->get('Grade');
-$Student_userID=$user->get('Student_userID');
-$sectionID=$user->get('sectionID');
 
-
-
+ // i changed the + to . i dont know why but its the only way that works.
 
 if(isset($_POST["button"]))
  {
  	//Update grades based on the students id#, whom are taking the same course
-    $sql=$db->prepare("UPDATE Grade FROM REGISTRATION.User WHERE Student_userID="+$Student_userID+"");
+    $sql=$db->prepare("UPDATE Grade FROM REGISTRATION.User WHERE Student_userID=".$Student_userID."");
     $sql->execute();
     
  }
  	//Select all the students who are in the same course
-	$coursesql=$db->prepare("SELECT Student_userID FROM Registration.Student_Grade_History WHERE CourseID="+$CourseID+"");
+ /*sql statement doesnt make sense. you need to find the courses that this prof teaches during semester/year(i think i need to talk to you in person about the semester year part):
+"SELECT * FROM Registration.Section s
+join Registration.Enrollment e
+on s.CourseID=e.CourseID and s.sectionID=e.sectionID 
+where Facutly_userID = '".$id."';"
+
+this statement joins Enrollment(e) and section (s) to get you all the courses this teacher teaches and students registered to the class
+	
+*/
+	$sql=$db->prepare("SELECT Student_userID FROM Registration.Student_Grade_History WHERE CourseID=".$CourseID."");
 	$sql->execute();
 	$result=$sql->fetchAll(PDO::FETCH_ASSOC);
+ 
 
-
-echo $twig->render('fgrade.html', array('id'=>$id, 'type' => $type, 'courseID' => $courseID, 'Grade' => $Grade, 'Student_userID' => $Student_userID, 'sectionID' => $sectionID 'result'=> $result ));
+//missing comma
+echo $twig->render('fgrade.html', array('id'=>$id, 'type' => $type, 'courseID' => $courseID, 'Grade' => $Grade, 'Student_userID' => $Student_userID, 'sectionID' => $sectionID, 'result'=> $result ));
 
 
 
--- finfo.php
+++ finfo.php
@@ -1,32 +1,26 @@
 <?php
-include_once "connection.php";
 require 'vendor/autoload.php';
+include_once "connection.php";
 $loader = new Twig_Loader_Filesystem("views");
-$twig = new Twig_Environment($loader);
+$twig = new Twig_Environment($loader, array('debug' => true));
+$twig->addExtension(new Twig_Extension_debug());
 $user = new Memcached();
 $id=$user->get('id');
 $type=$user->get('type');
-$Street=$user->get('Street');
-$City=$user->get('City');
-$Zip=$user->get('Zip');
-$State=$user->get('State');
-
  
-if(isset($_POST["submit"]))
+/*if(isset($_POST["submit"]))
 {
-    $sql=$db->prepare("UPDATE Street, City, Zip, State FROM REGISTRATION.User WHERE id="+$id+"");
+    $sql=$db->prepare("UPDATE Street, City, Zip, State FROM REGISTRATION.User WHERE id=".$id."");
     $sql->execute();
     
-}
+}*/
 
- 
-$sql=$db->prepare("SELECT * FROM Registration.User WHERE id="+$id+"");
+$sql=$db->prepare("SELECT * FROM Registration.User where userID='".$id."';");
 $sql->execute();
-$check = $sql->fetchAll(PDO::FETCH_ASSOC);
-
+$check=$sql->fetchAll(PDO::FETCH_ASSOC);
+ 
 
 
 
 echo $twig->render('finfo.html', array("id" =>$id, "type" => $type, "check" =>$check));
-
 ?>
