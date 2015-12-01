9e02e3a826f7aeef4e85c75e947a9916c64b5036
Daniel <daniel.kwak.dk@gmail.com>
2015-12-01 14:07:04 -0500

    DataTables Update

--- adddrop.php
+++ adddrop.php
@@ -27,6 +27,7 @@ $add=$_POST['addclass'];
 $drop=$_POST['removeclass'];
 
 if (isset($_POST['btn-submit'])) {
+//add/drop queries
 	if (isset($add)) {
 
 		foreach ($add as $key => $value) {
@@ -44,7 +45,7 @@ if (isset($_POST['btn-submit'])) {
 			$sql->execute();
 			$check2 = $sql->fetchAll(PDO::FETCH_ASSOC);
 
-			if ($check != null or $check2 > 4) {
+			if (count($check) > 0 or count($check2)+count($add) > 4) {
 				$err='You cannot register the same class more than once or register more than 4 classes.';
 			}else{
 				$sql=$db->prepare("INSERT INTO `Registration`.`Enrollment` (`sectionID`, `CourseID`, `Student_userID`) VALUES ('".$arr[1]."', '".$arr[0]."', '".$id."');");
@@ -53,32 +54,36 @@ if (isset($_POST['btn-submit'])) {
 		}
 
 	}elseif (isset($drop)) {
+
 		foreach ($drop as $key => $value) {
 			$arr=explode("-", $value);
 
 			$query=$db->prepare("DELETE FROM `Registration`.`Enrollment` WHERE `sectionID`='".$arr[1]."' and`CourseID`='".$arr[0]."' and`Student_userID`='".$id."';");
 			$query->execute();
 		}
+
 	}
 
 	if ($s==null or $y==null) {
 		$err='select BOTH semester and year';
 	}else{
-	$sql=$db->prepare("SELECT * FROM Registration.Enrollment e
-						INNER JOIN Registration.Section s 
-						ON (e.sectionID=s.sectionID and e.CourseID=s.CourseID)
-						INNER JOIN Registration.Course c
-						ON (c.CourseID=s.CourseID)
-						INNER JOIN Registration.User u
-	                    ON s.Faculty_userID=u.userID
-						 where e.Student_userID='".$id."' and s.semesterYear='".$y."' and s.semesterID='".$s."';");
-	$sql->execute();
-	$schedule=$sql->fetchAll(PDO::FETCH_ASSOC);
-
+		$sql=$db->prepare("SELECT * FROM Registration.Enrollment e
+							INNER JOIN Registration.Section s 
+							ON (e.sectionID=s.sectionID and e.CourseID=s.CourseID)
+							INNER JOIN Registration.Course c
+							ON (c.CourseID=s.CourseID)
+							INNER JOIN Registration.User u
+		                    ON s.Faculty_userID=u.userID
+							 where e.Student_userID='".$id."' and s.semesterYear='".$y."' and s.semesterID='".$s."';");
+		$sql->execute();
+		$schedule=$sql->fetchAll(PDO::FETCH_ASSOC);
 	}
+
 	echo $twig->render('Sschedule.html', array('id'=>$id,'email' => $email, 'fname' => $fname, 'lname' => $lname, 'type' => $type,
 											 'y' => $y, 's'=> $s, 'data'=>$schedule, 'err' => $err));
+
 }elseif(isset($_POST['btn-change'])){
+
 	$sql=$db->prepare("SELECT *
 						FROM Registration.Section s
 						INNER JOIN Registration.Course c
@@ -87,12 +92,13 @@ if (isset($_POST['btn-submit'])) {
 	                    ON s.Faculty_userID=u.userID where semesterYear='".$y."' and semesterID='".$s."';");
 	$sql->execute();
 	$result=$sql->fetchAll(PDO::FETCH_ASSOC);
+
 	echo $twig->render('add.html', array('id'=>$id,'email' => $email, 'fname' => $fname, 'lname' => $lname, 'type' => $type,
 											 'year' => $year, 'semester'=> $semester,'result' => $result, 'y'=> $y, 's'=>$s, 'err'=>$err));
 }else{
+
 	echo $twig->render('adddrop.html', array('id'=>$id,'email' => $email, 'fname' => $fname, 'lname' => $lname, 'type' => $type,
 											 'year' => $year, 'semester'=> $semester));
-
 }
 
 ?>
