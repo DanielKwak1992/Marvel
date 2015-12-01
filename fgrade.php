2ca124341d2ce0ca2931aab1554572cb7afb71fe
zachary chennis <zchennis32@gmail.com>
2015-12-01 14:26:09 -0500

    Trying to get grades page working, left comments

--- fgrade.php
+++ fgrade.php
@@ -1,30 +1,34 @@
 <?php
-include_once "connection.php";
+//include_once "connection.php";
 require 'vendor/autoload.php';
 $loader = new Twig_Loader_Filesystem("views");
 $twig = new Twig_Environment($loader);
 $user = new Memcached();
 $id=$user->get('id');
 $type=$user->get('type');
+$result=$user->get('result');
 $CourseID=$user->get('CourseID');
 $Grade=$user->get('Grade');
 $Student_userID=$user->get('Student_userID');
+$sectionID=$user->get('sectionID');
+
 
 
 
 if(isset($_POST["button"]))
  {
+ 	//Update grades based on the students id#, whom are taking the same course
     $sql=$db->prepare("UPDATE Grade FROM REGISTRATION.User WHERE Student_userID="+$Student_userID+"");
     $sql->execute();
     
  }
-
-	$sql=$db->prepare("SELECT Student_userID FROM Registration.Student_Grade_History WHERE CourseID="+$CourseID+"");
+ 	//Select all the students who are in the same course
+	$coursesql=$db->prepare("SELECT Student_userID FROM Registration.Student_Grade_History WHERE CourseID="+$CourseID+"");
 	$sql->execute();
 	$result=$sql->fetchAll(PDO::FETCH_ASSOC);
 
 
-echo $twig->render('fgrade.html', array('id'=>$id, 'type' => $type, 'result'=> $result ));
+echo $twig->render('fgrade.html', array('id'=>$id, 'type' => $type, 'courseID' => $courseID, 'Grade' => $Grade, 'Student_userID' => $Student_userID, 'sectionID' => $sectionID 'result'=> $result ));
 
 
 
