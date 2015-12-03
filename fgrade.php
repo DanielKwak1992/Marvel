210f5fa14f99b41f5be89854acecb6d1c3f76e82
zachary chennis <zchennis32@gmail.com>
2015-12-03 17:39:18 -0500

    Grades working just have to insert grades to database

--- fgrade.php
+++ fgrade.php
@@ -12,15 +12,63 @@ $fname=$user->get('fname');
 $lname=$user->get('lname');
 $type=$user->get('type');
 
+
+$semestersql=$db->prepare("SELECT * FROM Registration.Semester;");
+$semestersql->execute();
+$semester=$semestersql->fetchAll(PDO::FETCH_ASSOC);
+
+$yearsql=$db->prepare("SELECT semesterYear FROM Registration.Semester group by semesterYear;");
+$yearsql->execute();
+$year=$yearsql->fetchAll(PDO::FETCH_ASSOC);
+
+
+$y=$_POST['year'];
+$s=$_POST['semester'];
+$add=$_POST['addgrade'];
+
  // i changed the + to . i dont know why but its the only way that works.
 
-if(isset($_POST["button"]))
+if(isset($_POST["btn-submit"]))
  {
+//add grades
+
+			$sql=$db->prepare("SELECT * FROM Registration.Section WHERE Faculty_userID='".$id."' 
+						AND semesterYear='".$y."' and semesterID='".$s."';");
+			$sql->execute();
+			$course = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+
+    echo $twig->render('grade.html', array('id'=>$id,'email' => $email, 'fname' => $fname, 'lname' => $lname, 'type' => $type,
+											 'y' => $y, 's'=> $s, 'course'=>$course, 'err' => $err));
+
+} else if(isset($_POST["button"])) 
+{
+	$arr=explode("-",$_POST['course']);
+	$sql=$db->prepare("SELECT Student_userID, fName, lName FROM Registration.Enrollment e
+		join Registration.User u
+		on u.userID=e.Student_userID
+		WHERE sectionID=".$arr[1]." AND CourseID=".$arr[0].";");
+	$sql->execute();
+	$result=$sql->fetchAll(PDO::FETCH_ASSOC);
+ 
+
+
+ 	echo $twig->render('graderoster.html', array('id'=>$id,'email' => $email, 'fname' => $fname, 'lname' => $lname, 'type' => $type,
+											 'y' => $y, 's'=> $s, 'roster'=>$result,  'err' => $err));
+var_dump($result);
+} else if (false)
+{
+	
+}else{
+	echo $twig->render('fgrade.html', array('id'=>$id,'email' => $email, 'fname' => $fname, 'lname' => $lname, 'type' => $type,
+											 'year' => $year, 'semester'=> $semester));
+}
+
  	//Update grades based on the students id#, whom are taking the same course
-    $sql=$db->prepare("UPDATE Grade FROM REGISTRATION.User WHERE Student_userID=".$Student_userID."");
-    $sql->execute();
+   // $sql=$db->prepare("UPDATE Grade FROM REGISTRATION.User WHERE Student_userID=".$Student_userID."");
+    //$sql->execute();
     
- }
+ 
  	//Select all the students who are in the same course
  /*sql statement doesnt make sense. you need to find the courses that this prof teaches during semester/year(i think i need to talk to you in person about the semester year part):
 "SELECT * FROM Registration.Section s
@@ -31,17 +79,7 @@ where Facutly_userID = '".$id."';"
 this statement joins Enrollment(e) and section (s) to get you all the courses this teacher teaches and students registered to the class
 	
 */
-	$sql=$db->prepare("SELECT Student_userID FROM Registration.Student_Grade_History WHERE CourseID=".$CourseID."");
-	$sql->execute();
-	$result=$sql->fetchAll(PDO::FETCH_ASSOC);
- 
-
-//missing comma
-echo $twig->render('fgrade.html', array('id'=>$id, 'type' => $type, 'courseID' => $courseID, 'Grade' => $Grade, 'Student_userID' => $Student_userID, 'sectionID' => $sectionID, 'result'=> $result ));
-
-
-
-
+	
 
 
 ?>
