4c668399040d993370923b4f8cbae50d20452353
zachary chennis <zchennis32@gmail.com>
2015-12-03 11:40:39 -0500

    Minor add ons

---
+++ fschedule.php
@@ -0,0 +1,7 @@
+<?php
+include_once "connection.php";
+require 'vendor/autoload.php';
+$loader = new Twig_Loader_Filesystem("views");
+$twig = new Twig_Environment($loader);
+$user = new Memcached();
+//memcache only stores this data any other data you need will need to be queried
