b0ef3765bd0d0c67f5454a419d956b3677d1c7f1
zachary chennis <zchennis32@gmail.com>
2015-12-08 18:22:28 -0500

    ..

--- coursecatalog.php
+++ coursecatalog.php
@@ -1,11 +1,11 @@
 <?php
-//include_once "connection.php";
+include_once "connection.php";
 require 'vendor/autoload.php';
 $loader = new Twig_Loader_Filesystem("views");
 $twig = new Twig_Environment($loader);
 $user = new Memcached();
-//$id=$user->get('id');
-//$type=$user->get("type");
+$id=$user->get('id');
+$type=$user->get("type");
 $courseID=$user->('courseID');
 $courseName=$user->get('courseName');
 $credits=$user->get('credits');
@@ -13,17 +13,17 @@ $deptID=$user->get('deptID');
 
 
 
-echo $twig->render('coursecatalog.html', array("courseID" =>$courseID, "courseName" => $courseName, "credits" =>$courseName, "deptID" =>$deptID));
+echo $twig->render('coursecatalog.html', array("courseID" =>$courseID, "courseName" => $courseName, "credits" =>$courseName, "deptID" =>$deptID, "result" =>$result));
 
 $sql=$db->prepare("SELECT * FROM Registration.Course;");
 $sql->execute();
 
 $result=$sql->fetchAll(PDO::FETCH_ASSOC);
 
-if($result->num_rows > 0 {
-	while($row = 4result->fetch_assoc()){
-		echo "<br> major"
-	}
-})
+//if($result->num_rows > 0 {
+//	while($row = $result->fetch_assoc()){
+//		echo "<br> major"
+//	}
+//})
 
 ?>
