48505910e3ad5c81877915550dea4ed4d23fad1c
Daniel <daniel.kwak.dk@gmail.com>
2015-12-15 13:17:19 -0500

    addhold fix

--- addhold.php
+++ addhold.php
@@ -8,27 +8,27 @@ $user = new Memcached();
 $id=$user->get('id');
 $type=$user->get("type");
 $Student_userID= $_POST['Student_userID'];
-$TypeH= $_POST['TypeH'];
+$TypeH=$_POST['TypeH'];
 $found= false;
 
 $sql=$db->prepare("SELECT * FROM Registration.holds;");
 $sql->execute();
 $hold=$sql->fetchAll(PDO::FETCH_ASSOC);
 
-	if (isset($_POST['btn-submit'])){
-		if ($TypeH = 'Academic')
+if (isset($_POST['btn-submit'])){
+		if ($TypeH == 'Academic')
 		{
 				$acad = $db->prepare("UPDATE Registration.holds SET Academic = '1' WHERE Student_userID = '".$Student_userID."';");
 				$acad->execute();
 				$found= true;
 			}
-			elseif($TypeH ='Financial')
+			elseif($TypeH =='Financial')
 			{
 				$fin = $db->prepare("UPDATE Registration.holds SET Financial = '1' WHERE Student_userID = '".$Student_userID."';");
 				$fin->execute();
 				$found= true;
 			}
-			elseif($TypeH = 'Medical' )
+			elseif($TypeH == 'Medical' )
 			{
 				$med = $db->prepare("UPDATE Registration.holds SET Medical = '1' WHERE Student_userID = '".$Student_userID."';");
 				$med->execute();
@@ -38,5 +38,5 @@ $hold=$sql->fetchAll(PDO::FETCH_ASSOC);
 
 echo $twig->render('addhold.html', array("id" =>$id, "Student_userID"=> $Student_userID, "type" => $type));
 
-
+var_dump($TypeH=="Medical");
 ?>
