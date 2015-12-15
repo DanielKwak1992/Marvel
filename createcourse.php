a4d5a98908bc7d3aaeed9c5e701af4e077a08fd6
dejon vinson <dejonvinson@gmail.com>
2015-12-15 03:11:50 -0500

    constraints

--- createcourse.php
+++ createcourse.php
@@ -13,26 +13,31 @@ $courseNAme= $_POST['courseName'];
 $Credits= $_POST['Credits'];
 $deptID= $_POST['deptID'];
 $Type= $_POST['Type'];
+$changed = null;
 
 $test=$db->prepare("SELECT * FROM Registration.Course;");
 $test->execute();
 $course=$test->fetchAll(PDO::FETCH_ASSOC);
 
 
-		if (isset($_POST['btn-submit']) && ($CourseID != null) || ($CourseID != "CourseID" )){
+		if (isset($_POST['btn-submit'])){
+		if (($CourseID != "0") && ($CourseID != "CourseID" ) && ($courseName != "") && ($courseName != "courseName")){
 
 		$sql=$db->prepare ("Insert into Course (CourseID , courseName , Credits , deptID , Type )
 			VALUES ( '".$CourseID."' , '".$courseName."' , '".$Credits."' , '".$deptID."' , '".$Type."' );");
 			$sql->execute();
 			echo "Course successfully created";
+			$changed= true;
 		}
 			else {
 			echo "Error: " ; //need to output an error
+			$changed= false;
+	}
 	}
 
 
 
-echo $twig->render('createcourse.html', array("id" =>$id, "courseID"=> $CourseID, "type" => $type));
+echo $twig->render('createcourse.html', array("id" =>$id, "courseID"=> $CourseID, "type" => $type, "changed"=> $changed));
 
 
 
