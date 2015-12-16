a009dd9bd02cbcdc7fecb28ea24e41670ce2497e
dejon vinson <dejonvinson@gmail.com>
2015-12-16 03:28:51 -0500

    Visual changes and Section add

--- ecourse.php
+++ ecourse.php
@@ -1,21 +1,47 @@
 <?php
 require_once "connection.php";
 require 'vendor/autoload.php';
+require_once "aauthorize.php";
+
 $loader = new Twig_Loader_Filesystem("views");
 $twig = new Twig_Environment($loader);
 session_start();
 $id=$_SESSION['id'];
 $type=$_SESSION['type'];
 $userid = $_POST['userid'];
-$CourseID = $_POST['CourseID'];
+$CourseID =$_POST['CourseID'];
+$sectionID =$_POST['sectionID'];
+$roomNum =$_POST['roomNum'];
+$Faculty_userID =$_POST['Faculty_user'];
+$bldingName =$_POST['bldingName'];
+$timeSlot_Day =$_POST['timeSlot_Day'];
+$timeSlot_Time =$_POST['timeSlot_Time'];
+$semesterID =$_POST['semesterID'];
+$semesterYear =$_POST['semesterYear'];
+$changed =null;
 
 
-		$sql=$db->prepare("SELECT * FROM Registration.Course;");
+		$sql=$db->prepare("SELECT * FROM Registration.Section;");
 			$sql->execute();
 			$edit=$sql->fetchAll(PDO::FETCH_ASSOC);
+			
+		if (isset($_POST['btn-submit'])){
+		if (($CourseID != "0") && ($CourseID ="CourseID") && ($timeslot_Time != "timeSlot_Time"){
 
+		$sql=$db->prepare ("Insert into Section (CourseID , sectionID , roomNum , Faculty_userID , bldingName , timeSlot_Day , timeSlot_Time, semesterID , semesterYear)
+							VALUES ( '".$CourseID."' , '".$sectionID."' , '".$roomNum."' , '".$Faculty_userID."' , '".$bldingName."', '"$timeSlot_Day"',
+'"$timeSlot_Time"' , '"$semesterID"' , '"$semesterYear"');");
+			$sql->execute();
+			echo "Course successfully created";
+			$changed= true;
+		}
+			else {
+			echo "Error: " ; //need to output an error
+			$changed= false;
+	}
+	}
 
-echo $twig->render('editcourse.html', array("id" =>$id, "courseID"=> $CourseID, "type" => $type));
+echo $twig->render('editcourse.html', array("id" =>$id, "courseID"=> $CourseID, "type" => $type, "changed" => $changed));
 
 
 
--- removehold.php
+++ removehold.php
@@ -5,6 +5,7 @@ require_once "aauthorize.php";
 
 $loader = new Twig_Loader_Filesystem("views");
 $twig = new Twig_Environment($loader);
+$user = new Memcached();
 session_start();
 $id=$_SESSION['id'];
 $type=$_SESSION['type'];
@@ -19,25 +20,25 @@ $hold=$sql->fetchAll(PDO::FETCH_ASSOC);
 if (isset($_POST['btn-submit'])){
 		if ($TypeH == 'Academic')
 		{
-				$acad = $db->prepare("UPDATE Registration.holds SET Academic = '1' WHERE Student_userID = '".$Student_userID."';");
+				$acad = $db->prepare("UPDATE Registration.holds SET Academic = '0' WHERE Student_userID = '".$Student_userID."';");
 				$acad->execute();
 				$found= true;
 			}
 			elseif($TypeH =='Financial')
 			{
-				$fin = $db->prepare("UPDATE Registration.holds SET Financial = '1' WHERE Student_userID = '".$Student_userID."';");
+				$fin = $db->prepare("UPDATE Registration.holds SET Financial = '0' WHERE Student_userID = '".$Student_userID."';");
 				$fin->execute();
 				$found= true;
 			}
 			elseif($TypeH == 'Medical' )
 			{
-				$med = $db->prepare("UPDATE Registration.holds SET Medical = '1' WHERE Student_userID = '".$Student_userID."';");
+				$med = $db->prepare("UPDATE Registration.holds SET Medical = '0' WHERE Student_userID = '".$Student_userID."';");
 				$med->execute();
 				$found= true;
 				}
 }
 
-echo $twig->render('addhold.html', array("id" =>$id, "Student_userID"=> $Student_userID, "type" => $type));
+echo $twig->render('removehold.html', array("id" =>$id, "Student_userID"=> $Student_userID, "type" => $type));
 
 var_dump($TypeH=="Medical");
 ?>
